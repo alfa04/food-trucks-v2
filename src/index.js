60520b868c72e1ec8cb9e260ffa201917abe7f6f
Joao Costa.ext <joao.costa.ext@teamviewer.com>
2024-05-26 23:19:39 +0100

    feat: Remove react router dom since I'm not actually using it

--- src/index.js
+++ src/index.js
@@ -3,22 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+//import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
-const router = createBrowserRouter([
+/*const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
     },
-]);
+]);*/
 
 // Improve the routes
+//      <RouterProvider router={router} />
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-      <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
