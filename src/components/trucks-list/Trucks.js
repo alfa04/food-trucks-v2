517d6c0b40e899449d070ecac9a192ecf3c9a368
Joao Costa.ext <joao.costa.ext@teamviewer.com>
2024-05-27 04:02:31 +0100

    chore: Remove not needed comments

--- src/components/trucks-list/Trucks.js
+++ src/components/trucks-list/Trucks.js
@@ -9,14 +9,6 @@ const Trucks = ({errorSimulationStatus, initialPage}) => {
     const [page, setPage] = useState(initialPage);
     const [itemsPerPage] = useState(10); // Add feature to make number of items dynamic
     const {data, isLoading} = useGetTrucks(page, itemsPerPage, errorSimulationStatus);
-    //return <LoadingSpinner />
-
-    /*dd nomponent to naviga, sisable control when no more pages, and change de number of truck per page, filter just truck with button instead???*/
-//Add use key for the key
-    //- 481 -return empty the query
-    //esta a partir quando nao carrega e fazemos refresh, temos de resetar qquer coisa, ver kent, remover a validação de data, tem de ser um reset
-
-//<ErrorPage message={error}>Error</ErrorPage>
 
     return (
         isLoading ? <div className="loadingSpinner"><LoadingSpinner/></div> :
